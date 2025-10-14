f6370465ee4d91d1271becd70bfa910ca28f28d8
lusalord <lusalord63@gmail.com>
2025-10-14 12:25:11 +0900

    fix : Player Animation

--- Assets/00.Work/Lusalord/02.Script/Player/PlayerAnimation.cs
+++ Assets/00.Work/Lusalord/02.Script/Player/PlayerAnimation.cs
@@ -1,34 +1,35 @@
-using System;
 using _00.Work.Lusalord._02.Script.Agent;
-using _00.Work.Lusalord._02.Script.Player;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+namespace _00.Work.Lusalord._02.Script.Player
 {
-    #region Component
-        private Animator _animator;
-        private Player _player;
-    #endregion
-
-    #region Hash
-        private readonly int _moveXHash = Animator.StringToHash("MoveX");
-        private readonly int _moveYHash = Animator.StringToHash("MoveY");
-    #endregion
-
-    private void Awake()
+    public class PlayerAnimation : MonoBehaviour
     {
-        _animator = GetComponent<Animator>();
-        _player = GetComponentInParent<Player>();
-    }
+        #region Component
+            private Animator _animator;
+            private Player _player;
+        #endregion
 
-    private void FixedUpdate()
-    {
-        SetAnimation(_player.MovementCompo);
-    }
+        #region Hash
+            private readonly int _moveXHash = Animator.StringToHash("MoveX");
+            private readonly int _moveYHash = Animator.StringToHash("MoveY");
+        #endregion
 
-    private void SetAnimation(AgentMovement movement)
-    {
-        _animator.SetFloat(_moveXHash, Mathf.Abs(movement.Rb.linearVelocityX));
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _player = GetComponentInParent<Player>();
+        }
+
+        private void FixedUpdate()
+        {
+            SetAnimation(_player.MovementCompo);
+        }
+
+        private void SetAnimation(AgentMovement movement)
+        {
+            _animator.SetFloat(_moveXHash, Mathf.Abs(movement.Rb.linearVelocityX));
+        }
     }
 }
 
