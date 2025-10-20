08f0ffb6843a4909493c4027d682966f0c622d13
lusalord <lusalord63@gmail.com>
2025-10-20 15:18:36 +0900

    feat : font

---
+++ Assets/00.Work/Lusalord/02.Script/VisualNovel/DialogueData/DialogueData.cs
@@ -0,0 +1,10 @@
+namespace _00.Work.Lusalord._02.Script.VisualNovel.AttributeData
+{
+    [System.Serializable]
+    public class DialogueData
+    {
+        public string speaker;
+        public string text;
+        public string eventTag;
+    }
+}
