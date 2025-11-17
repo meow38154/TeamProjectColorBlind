72d7de222e5bcde36901ae1d2ec18dc8c6560032
lusalord <lusalord63@gmail.com>
2025-11-17 14:13:56 +0900

    feat : Manager, DamageCaster

--- Assets/00.Work/Lusalord/02.Script/Player/Player.cs
+++ Assets/00.Work/Lusalord/02.Script/Player/Player.cs
@@ -78,10 +78,5 @@ namespace _00.Work.Lusalord._02.Script.Player
         {
             PlayerInput.jumpPressedFlag = false;
         }
-
-        private void OnCollisionEnter2D(Collision2D other)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
