e0cadfacf75c6a2155f3198f8bc11c7b16838a7d
lusalord <lusalord63@gmail.com>
2025-11-23 21:18:50 +0900

    feat : Build Scene

--- Assets/00.Work/HJW/02.Script/UI/ButtonEvent.cs
+++ Assets/00.Work/HJW/02.Script/UI/ButtonEvent.cs
@@ -1,28 +1,42 @@
+using System;
+using JJM;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonEvent : MonoBehaviour
 {
     [SerializeField] private string NextSceneName;
     [SerializeField] private GameObject OptionPanel;
+    [SerializeField] private Slider slider;
+
+    private void Update()
+    {
+        SaveManager.Instance.Data.soundSetting = slider.value;
+    }
+
     public void StartButton()
     {
         SceneManager.LoadScene(NextSceneName);
     }
 
+    public void Slider()
+    {
+    }
     public void OptionButton()
     {
         OptionPanel.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void QuitButton()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 
     public void OptionQuitButton()
     {
+        Time.timeScale = 1;
         OptionPanel.SetActive(false);
     }
 }
