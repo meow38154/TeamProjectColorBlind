1f5af0011277a354f9ed770f180b3aa2556e0526
lusalord <lusalord63@gmail.com>
2025-11-23 22:55:21 +0900

    fix : Start Scene

---
+++ Assets/00.Work/Lusalord/02.Script/Tutorial.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject tutorial;
+
+    private void Awake()
+    {
+        tutorial.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+            tutorial.SetActive(!tutorial.activeSelf);
+        }
+    }
+}
